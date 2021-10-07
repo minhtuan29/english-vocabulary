f782c43570ff2a530433df1e9c1adf31c835c61b
minhtuan29 <86332370+minhtuan29@users.noreply.github.com>
2021-10-07 20:07:19 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,166 @@
+
+//===========================CONFIG FILE=================================
+const DELAY_TIME_OUT = 300;
+const ENTER_KEY = 13;
+const ENOUGH_CORRECT_TIME = 2;
+const SHIFT_KEY = 16;
+const CTRL_KEY = 17;
+
+const VOLUME = 5;
+const VOICE_URI = "native";
+const PITCH_AUDIO = 1;
+const RATE_AUDIO = 1; 
+const SPEAKER_LANGUAGE = 'en-US';
+
+
+let VOCABULARIES;
+
+const vocabFileReader = new FileReader();
+
+
+
+vocabFileReader.onload = () =>{
+  VOCABULARIES = vocabFileReader.result.split('\r\n').map(getVocByLine);
+  renderVocab(); 
+  $(".file").css("display", "none");
+  $(".container").css("display", "block");
+  $(".input-text").focus();
+  document.getElementById("tutorial-el").innerText = "";
+}
+
+const inputTxtFile = document.querySelector('input[type="file"]');
+inputTxtFile.addEventListener(
+  'change',
+  () => vocabFileReader.readAsText(inputTxtFile.files[0])
+);
+
+
+let viMeaningHTML = document.getElementById("vi");
+
+let currentVocabulary;
+let correctInputTime = 0;
+
+//==========================RUN FILE============================================
+
+
+$(document).on(
+  "keyup",
+  $('input[type="text"]'),
+  (userInput) =>{
+    if (userInput.keyCode === SHIFT_KEY) {
+      $("#vi").slideToggle();
+    }
+  }
+);
+
+
+
+$(document).on(
+  "keyup",
+  $('input[type="text"]'),
+  (userInput) =>{
+    if (userInput.keyCode === CTRL_KEY) {
+      Speaker.say(currentVocabulary.eng);
+    }
+  }
+);
+
+
+
+$(document).on(
+  "keyup",
+  $('input[type="text"]'),
+  (userInput) =>{
+    if (userInput.keyCode === ENTER_KEY) {
+      currentVocabularyCompareWith($('input[type="text"]').val());
+      resetUserInputForm();
+      if (correctInputTime === ENOUGH_CORRECT_TIME) {
+        goToNextRandomVocabulary();
+      }
+    }
+  }
+);
+
+
+
+//==========================PROGRAM FILE==================================
+
+function getVocByLine(line){
+  let words = line.split(': ');
+  return {eng: words[0], vi: words[1]};
+}
+
+
+
+class Speaker{
+  static main = new SpeechSynthesisUtterance();
+  static config(){
+    Speaker.main.voiceURI = VOICE_URI;
+    Speaker.main.volume = VOLUME;
+    Speaker.main.rate = RATE_AUDIO;
+    Speaker.main.pitch = PITCH_AUDIO;
+    Speaker.main.lang = SPEAKER_LANGUAGE;
+  }
+  static say(msg){
+    Speaker.main.text = msg;
+    window.speechSynthesis.speak(Speaker.main);
+  }
+}
+
+
+
+
+function getRandomVocab() {
+  return VOCABULARIES[Math.floor(Math.random() * VOCABULARIES.length)];
+} 
+
+
+
+
+function goToNextRandomVocabulary () {
+  setTimeout(
+    () => {
+      currentVocabulary = getRandomVocab();
+      correctInputTime = 0;
+      $(".vocab").html(currentVocabulary.eng);
+      viMeaningHTML.innerText = currentVocabulary.vi;
+    },
+    DELAY_TIME_OUT
+  );
+}
+
+
+
+function resetUserInputForm () {
+  setTimeout(
+    () =>{
+      $(".vocab").removeClass("wrong-red right-green");
+      $("input").val("");
+    },
+    DELAY_TIME_OUT
+  );
+}
+
+
+
+function currentVocabularyCompareWith (userInputValue) {
+  if (userInputValue=== currentVocabulary.eng.toLowerCase() ) {
+    correctInputTime++;
+    responsiveVoice.speak(currentVocabulary.eng ); // đây nè
+    // Speaker.say(currentVocabulary.eng);. hú hú bé tâm ơi
+    $(".vocab").addClass("right-green");
+  }
+  else {
+    $(".vocab").addClass("wrong-red");
+  }
+}
+
+let voicelist = responsiveVoice.getVoices();
+console.log(voicelist);
+
+
+function renderVocab () {
+  currentVocabulary = getRandomVocab();
+  $(".vocab").html(currentVocabulary.eng);
+  viMeaningHTML.innerText = currentVocabulary.vi;
+}
